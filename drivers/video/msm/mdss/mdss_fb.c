639277934044f21a209dfb296917238ddb71710c
Shivaraj Shetty <shivaraj@codeaurora.org>
2014-06-10 21:28:36 +0530

    msm: mdss: Avoid mutex lockup in dynamic switching
    Calling blank/unblank after locking the backlight mutex will result
    in mutex lockup as blank/unblank would again try to aquire the same
    lock. Avoid this situation in Dynamic Switching
    
    Change-Id: I7b5e72b964b9994d98b6a3d0771239a5423c5001
    Signed-off-by: Shivaraj Shetty <shivaraj@codeaurora.org>

--- drivers/video/msm/mdss/mdss_fb.c
+++ drivers/video/msm/mdss/mdss_fb.c
@@ -454,6 +454,7 @@ static int mdss_fb_lpm_enable(struct msm_fb_data_type *mfd, int mode)
 	mutex_lock(&mfd->bl_lock);
 	bl_lvl = mfd->bl_level;
 	mdss_fb_set_backlight(mfd, 0);
+	mutex_unlock(&mfd->bl_lock);
 
 	lock_fb_info(mfd->fbi);
 	ret = mdss_fb_blank_sub(FB_BLANK_POWERDOWN, mfd->fbi,
@@ -461,7 +462,6 @@ static int mdss_fb_lpm_enable(struct msm_fb_data_type *mfd, int mode)
 	if (ret) {
 		pr_err("can't turn off display!\n");
 		unlock_fb_info(mfd->fbi);
-		mutex_unlock(&mfd->bl_lock);
 		return ret;
 	}
 
@@ -477,11 +477,11 @@ static int mdss_fb_lpm_enable(struct msm_fb_data_type *mfd, int mode)
 	if (ret) {
 		pr_err("can't turn on display!\n");
 		unlock_fb_info(mfd->fbi);
-		mutex_unlock(&mfd->bl_lock);
 		return ret;
 	}
 	unlock_fb_info(mfd->fbi);
 
+	mutex_lock(&mfd->bl_lock);
 	mfd->bl_updated = true;
 	mdss_fb_set_backlight(mfd, bl_lvl);
 	mutex_unlock(&mfd->bl_lock);
