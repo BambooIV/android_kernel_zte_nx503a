c49a0a1607b14f9c4a3faaf178749c48a9f56ef1
Vineet Bajaj <vbajaj@codeaurora.org>
2014-11-20 18:39:47 +0530

    msm: mdss: fix underrun seen with HDMI connected as external
    Underruns are observed on 8974 with HDMI connected since the
    combined BW of primary and HDMI interface exceeds the
    threshold BW. Fail the prepare call under such conditions so
    that fallback happens.
    
    Change-Id: I1217c862c344871868e1fabbb7ba51f6814c1e04
    Signed-off-by: Vineet Bajaj <vbajaj@codeaurora.org>
    Signed-off-by: Raghavendra Ambadas <rambad@codeaurora.org>

--- drivers/video/msm/mdss/mdss_mdp_overlay.c
+++ drivers/video/msm/mdss/mdss_mdp_overlay.c
@@ -1225,6 +1225,7 @@ int mdss_mdp_overlay_kickoff(struct msm_fb_data_type *mfd,
 		mutex_lock(ctl->shared_lock);
 
 	mutex_lock(&mdp5_data->ov_lock);
+	ctl->bw_pending = 0;
 	mutex_lock(&mdp5_data->list_lock);
 
 	/*
